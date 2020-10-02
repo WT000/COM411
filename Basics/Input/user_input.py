f38e84393ff711a087f97f24721cc636be9f4b60
WT000 <55751558+WT000@users.noreply.github.com>
2020-10-02 12:09:51 +0000

    Coded user input and an associated output

---
+++ Basics/Input/user_input.py
@@ -0,0 +1,10 @@
+# Ask the user what their name is
+name = input("What is your name, human?" + "\n")
+
+# Print the response
+print("It's nice to meet you human,", name + "!")
+
+################
+print("\nOr, we could also do... \n")
+
+print("It's nice to meet you, {}!".format(name))
