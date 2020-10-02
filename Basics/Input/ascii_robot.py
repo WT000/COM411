cd1004483d664d96aa0808c6ccc707761a974268
WT000 <55751558+WT000@users.noreply.github.com>
2020-10-02 12:17:53 +0000

    Coded dynamic eyes for the ASCII robot

---
+++ Basics/Input/ascii_robot.py
@@ -0,0 +1,15 @@
+beep_eye = input("Please enter a character for Beep's eyes\n")
+
+
+print("##########")
+print("#        #")
+print("# " + beep_eye + "    " + beep_eye + " #")
+print("#   --   #")
+print("##########")
+print("   ####")
+print("##########")
+print("#  ####  #")
+print("#  ####  #")
+print("   ####")
+print("   ####")
+print("    ##")
