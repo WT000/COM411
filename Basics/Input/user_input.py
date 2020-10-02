be01549f9c349c7ee794508d51bf1ef8dacb8791
WT000 <55751558+WT000@users.noreply.github.com>
2020-10-02 13:44:34 +0000

    Additional comments

--- Basics/Input/user_input.py
+++ Basics/Input/user_input.py
@@ -1,5 +1,5 @@
 # Ask the user what their name is
-name = input("What is your name, human?" + "\n")
+name = input("What is your name, human?\n")
 
 # Print the response
 print("It's nice to meet you human,", name + "!")
@@ -8,3 +8,6 @@ print("It's nice to meet you human,", name + "!")
 print("\nOr, we could also do... \n")
 
 print("It's nice to meet you, {}!".format(name))
+
+# The users input is stored in memory, "name" itself is stored in memory using binary, by being called "name" it makes it easier for us to remember.
+# Using a + is concatenation. Using , is specific to python, using + is in almost every language.
