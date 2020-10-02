7d2cbe88ccc1fef6b6fd881f2512d09383f22033
WT000 <55751558+WT000@users.noreply.github.com>
2020-10-02 13:23:38 +0000

    Alternative solution to the problem

--- Basics/Output/escape_characters.py
+++ Basics/Output/escape_characters.py
@@ -6,3 +6,5 @@ print("\" Displays a double quote")
 print("\' Displays a single quote", "\n")
 
 print("\tI am Beep!", "\n")
+
+# Use 'this' instead of "this" and you can use quotes in a string without needing to do \" or \'!
