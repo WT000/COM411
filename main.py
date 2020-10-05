96ec3263c59e94d4303f03bc84e5fae7278f04d7
WT000 <55751558+WT000@users.noreply.github.com>
2020-10-05 13:42:25 +0000

    Added a code page, this will be added to as I progress through the course and want to demonstrate code.

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello")
+# This is going to be used as a command index, allowing me to refer to it if I get confused.
+
+# 1 - Basics
+print("Hello!")
+print("\n\tFancy Hello with a new line and tab!\n")
+
+greeting = "\"Hello\""
+greeting2 = "Hello2"
+print(greeting)
+
+print("{} is greeting, {} is greeting 2.".format(greeting, greeting2))
+
+greeting3 = float(input("Enter some numbers here, include some decimal places since this is a float!\n"))
+print(str(greeting3))
+# without string also works
+# to limit deciman places
+print("{:.2f}".format(greeting3))
+
+greeting4 = int(input("Enter a whole number\n"))
+greeting5 = greeting4 * (1+1)
+
+print(greeting5)
+print("*" * greeting5)
