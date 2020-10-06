ab77163bf2f3d37841a39a2ef5c3b640fa4a6cdc
WT000 <55751558+WT000@users.noreply.github.com>
2020-10-06 09:28:49 +0000

    Coded using a modulus operator to work out an even oro dd number

---
+++ Basics/decisions/simple_decision/modulo_operator.py
@@ -0,0 +1,8 @@
+# Ask the user to enter a whole number.
+number = int(input("Please enter a whole number.\n"))
+
+# Work out if the number is even or odd
+if (number % 2 == 0):
+  print("This number is even.")
+else:
+  print("This number is odd.")
