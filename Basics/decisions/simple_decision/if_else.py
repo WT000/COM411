71463e9f38038eadc6fc0793e6edc5d34a07ee5e
WT000 <55751558+WT000@users.noreply.github.com>
2020-10-06 09:14:38 +0000

    Coded an appropriate if and else statement

---
+++ Basics/decisions/simple_decision/if_else.py
@@ -0,0 +1,11 @@
+# Ask the user for the activity
+activity = input("Please enter the activity to be performed.\n")
+
+# Output an appropriate response based on user input
+if (activity == "calculate"):
+  print("Performing calculations...")
+else:
+  print("Performing activity...")
+
+# Complete the program
+print("Activity completed!")
