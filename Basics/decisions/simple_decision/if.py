713ddbc736889d6a32fceab0a32e621521d1eed3
WT000 <55751558+WT000@users.noreply.github.com>
2020-10-06 09:09:45 +0000

    Coded a simple if decision

---
+++ Basics/decisions/simple_decision/if.py
@@ -0,0 +1,9 @@
+# Get the book from the user
+book_type = input("What type of book is this?\n")
+
+# Check book_type and see if it's an adventure book
+if (book_type == "adventure"):
+  print("I like adventure books!")
+
+# Finish the program
+print("Finished reading book.")
