4573005fe006033e7329f37943d19efe280fa897
WT000 <55751558+WT000@users.noreply.github.com>
2020-10-13 11:11:35 +0000

    Added alternative solution in comment and also implemented abs function

--- Basics/functions/ascii_character.py
+++ Basics/functions/ascii_character.py
@@ -1,8 +1,10 @@
 # Ask the user for an int
 print("Program started!")
-ascii_code = int(input("Please enter an ASCII code:"))
+ascii_code = abs(int(input("Please enter an ASCII code: ")))
+# abs makes it a whole number
 
 # Check if the input is within the ASCII range (32 - 126) and then convert it, else it will display a suitable error message
+# could also be done by number => 36 and number <= 126
 if (ascii_code in range(32, 126)):
   print("The character represented by the ASCII code {} is: {}".format(ascii_code, chr(ascii_code)))
 else:
