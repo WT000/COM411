324d7398b807e9e5fa0bfeb1a15148bdb0a65b93
WT000 <55751558+WT000@users.noreply.github.com>
2020-10-20 09:02:41 +0000

    Added code to demonstrate a simple list.

---
+++ Data/lists/simple.py
@@ -0,0 +1,11 @@
+# Directions function that creates and returns the direction list
+def directions():
+  directions = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
+  return directions
+
+# Run function that runs and displays the function
+def run():
+  print(directions())
+
+# Run the program
+run()
