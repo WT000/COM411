42e86a0d9b5b1f175889cf6afe91bb0fb5a41930
WT000 <55751558+WT000@users.noreply.github.com>
2020-10-20 09:46:26 +0000

    Added comments to iterate_list

--- Data/lists/iterate_list.py
+++ Data/lists/iterate_list.py
@@ -1,15 +1,19 @@
+# Create the list with the desired directions
 def directions():
   directions = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
   return directions
 
+# Menu function which goes through each index in the list
 def menu():
-  print("Please enter a direction:")
+  print("Please enter a direction:") # (won't have functionality)
   direction_list = directions()
 
-  for count in range(len(direction_list)):
-    print("{}: {}".format(count, direction_list[count]))
+  for index in range(len(direction_list)):
+    print("{}: {}".format(index, direction_list[index]))
 
+# Function which calls the menu
 def run():
   menu()
 
+# Call the run function
 run()
