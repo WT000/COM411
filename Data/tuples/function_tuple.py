0589535b9f0e7e41314ed834343d2e8ce72472a3
WT000 <55751558+WT000@users.noreply.github.com>
2020-10-20 11:27:38 +0000

    Comments didn't save, they're now completed.

--- Data/tuples/function_tuple.py
+++ Data/tuples/function_tuple.py
@@ -4,9 +4,11 @@ def likelihood():
   likelihoods = (50, 38, 27, 99, 4)
   return (min(likelihoods), max(likelihoods))
 
-# Run function which prints t
+# Run function which prints the minimum and maximum values from
+# the tuple
 def run():
   acquired = likelihood()
   print("Minimum likelihood of falling: {}% \nMaximum likelihood of falling: {}%".format(acquired[0], acquired[1]))
 
+# Run the program
 run()
