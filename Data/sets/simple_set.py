853943d468edf74e329f4565585d33b404374845
WT000 <55751558+WT000@users.noreply.github.com>
2020-10-27 10:08:49 +0000

    Coded a simple set

---
+++ Data/sets/simple_set.py
@@ -0,0 +1,11 @@
+# Observed function which takes 0 parameters and returns a set
+def observed():
+  observations = {"Flying Car", "Sky Scraper", "Laser", "Dome"}
+  return observations
+
+# Run which will call the observed function and print it
+def run():
+  print(observed())
+
+# Run the program
+run()
