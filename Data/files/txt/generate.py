156261009980066665a4173452d5f6c053f4641f
WT000 <55751558+WT000@users.noreply.github.com>
2020-11-03 21:34:23 +0000

    Added comments to help me code at a later date

--- Data/files/txt/generate.py
+++ Data/files/txt/generate.py
@@ -1,9 +1,16 @@
+import csv
+
 def search(fileName):
   print("Searching...")
   data = {}
 
   with open(fileName) as file: 
-    
+  
+  # split
+  # for data in file
+  # startswith
+  # strip
+  # each dict value is an array, make an empty array if the dict list doesnt already exist, else append it
   
   print("Done!")
   return data
@@ -11,4 +18,6 @@ def search(fileName):
 def run():
   data = search("Data/files/txt/books.txt")
 
+#perhaps use a loop within a loop? goes into array and then loops
+#through each item in the array to print it
 run()
