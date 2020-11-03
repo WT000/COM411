14e9ad8204ce2cce63926536447f884cbcec9f6a
WT000 <55751558+WT000@users.noreply.github.com>
2020-11-03 10:54:26 +0000

    Coded a program that reads a txt file

---
+++ Data/files/txt/read.py
@@ -0,0 +1,28 @@
+# Import os to use specific functions
+import os
+
+# Search function which includes one parameter, that being
+# the directory of a file
+def search(fileDirectory):
+  # Print the working directory and what's inside it
+  print("The current working directory is: {}".format(os.getcwd()))
+  
+  print("And what's in the working directory is:")
+  for data in os.listdir():
+    print(data)
+  
+  # In a new section, go through the txt file and print every line
+  # inside it
+  print()
+  print("Searching...")
+  with open(fileDirectory) as file:
+    for line in file:
+      print("Looked in {}".format(line), end="")
+    print()
+
+# Run function which calls the search function with a directory
+def run():
+  search("Data/files/txt/locations.txt")
+
+# Run the program
+run()
