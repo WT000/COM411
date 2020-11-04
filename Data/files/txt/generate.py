7aee4a48a35da9eb18ca6f4c4dc82590196f8703
WT000 <55751558+WT000@users.noreply.github.com>
2020-11-04 12:49:12 +0000

    Finished coding a program that generates csv files

--- Data/files/txt/generate.py
+++ Data/files/txt/generate.py
@@ -1,23 +1,49 @@
-import csv
-
+# Search function which takes one parameter, that being the
+# directory of the file to search for
 def search(fileName):
   print("Searching...")
+  
+  # Create an empty dict
   data = {}
 
+  # Open the file for reading (no arguments needed)
   with open(fileName) as file: 
-  
-  # split
-  # for data in file
-  # startswith
-  # strip
-  # each dict value is an array, make an empty array if the dict list doesnt already exist, else append it
-  
-  print("Done!")
-  return data
+    for line in file:
+      line = line.strip()
 
+      # If a line startswith with "Section", split at : and
+      # make the variable equal the name of the section
+      # (located in the the list which .split creates)
+      if (line.startswith("Section")):
+        section = line.split(":")
+        section = section[1]
+
+      # If it doesn't start with section it's a book, if
+      # the section doesn't exist add it and the stored
+      # section and book to the dict, else append the
+      # new book if the section already exists
+      else:
+        if (section in data):
+          data[section].append(line)
+        else:
+          data[section]=[line]
+
+    # Return the dict to the main program
+    return data
+
+# Run function which takes no parameters
 def run():
   data = search("Data/files/txt/books.txt")
 
-#perhaps use a loop within a loop? goes into array and then loops
-#through each item in the array to print it
+  # This uses a nested loop, it loops through the number of keys
+  # (sections) in the dict and then loops through the number of
+  # books (values) inside the associated list
+  with open("Data/files/txt/generated.csv", "w") as file:
+    for section in data.items():
+      for book in section[1]:
+        file.write("{},{}".format(section[0], book + "\n"))
+  
+  print("Done!")
+
+# Run the program
 run()
