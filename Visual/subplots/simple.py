20703aa1703f5b91fe9c7386056ed778c67ba9b6
WT000 <55751558+WT000@users.noreply.github.com>
2020-11-17 10:51:45 +0000

    Coded a program which shows data in different formats

---
+++ Visual/subplots/simple.py
@@ -0,0 +1,36 @@
+# Importing the pyplot module from matplotlib
+import matplotlib.pyplot as plt
+
+# Read data function which takes one parameter, the file path of the data
+def read_data(file_path):
+  # Read the file and append the data to a list which is then returned out of
+  # the program
+  with open(file_path) as file:
+    data = []
+    
+    for line in file:
+      line = line.strip()
+      data.append(line)
+    
+    return data
+
+# Run function which takes no parameters
+def run():
+  # Call the read data function and store it in a data variable
+  data = read_data("Visual/subplots/temps.txt")
+
+  # Create the subplots which share the same x axis
+  fig, axs = plt.subplots(2, sharex="all")
+
+  # Figure title
+  fig.suptitle("Temperature Data")
+  
+  # Plot the two pieces of data in different formats
+  axs[0].plot(data, "bo-")
+  axs[1].bar(range(len(data)), data)
+  
+  # Show the plot now that it's created
+  plt.show()
+
+# Run the program
+run()
