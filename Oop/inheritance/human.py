65b7f9754fa32b4d9a96adbdb95bb563299fb3bb
WT000 <55751558+WT000@users.noreply.github.com>
2020-12-08 10:53:21 +0000

    Added human class comments

--- Oop/inheritance/human.py
+++ Oop/inheritance/human.py
@@ -1,14 +1,22 @@
+# Import the Inhabitant class which will be used as the super class
 from inhabitant import Inhabitant
 
+# When creating the human class, we now have to specify that it's a sub class of the
+# Inhabitant class
 class Human(Inhabitant):
   def __init__(self, name="Human", age=0):
+    # By using super(), we now run the __init__ function inside the inhabitant class, which
+    # allows us to run eat() and move() within this class, even though it's not here!
     super().__init__()
+    # Once done, we now overwrite the name and age with the ones specified by the user
     self.name = name
     self.age = age
 
+  # Sub class method which only humans can do
   def display(self):
     print("Hello! My name is {} and I'm a human!".format(self.name))
 
+  # Overwrite the __str__ and __repr__ magic methods
   def __str__(self):
     return "My name is {} and I'm a human, I'm currently {} with an energy of {}.".format(self.name, self.age, self.energy)
 
